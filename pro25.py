76caf09c91097501f3fab23a095295cdaefa5684
BrahminiA <43632503+BrahminiA@users.noreply.github.com>
2019-07-16 11:27:04 +0530

    Add files via upload

---
+++ pro25.py
@@ -0,0 +1,18 @@
+def lenof(arr, n):
+    max1 = 1
+    len1 = 1
+    for i in range(1, n):
+
+        if (arr[i] > arr[i - 1]):
+            len1 = len1 + 1
+        else:
+            if (max1 < len1):
+                max1 = len1
+            len1 = 1
+    if (max1 < len1):
+        max1 = len1
+    return max1
+
+n = int(input())
+L = [ int(x) for x in input().split()]
+print(lenof(L, n))
