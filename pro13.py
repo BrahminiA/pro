49f13cc391c9b80c0f6757ed6b4145535777b114
BrahminiA <43632503+BrahminiA@users.noreply.github.com>
2019-07-16 10:13:22 +0530

    Add files via upload

--- pro13.py
+++ pro13.py
@@ -1,15 +1,8 @@
-n,q=input().split()
-n=int(n)
-q=int(q)
-result=[]
-l=list(map(int,input().split()))
-for i in range(0,q):
-    u,v=input().split()
-    u=int(u)
-    v=int(v)
-    k=0
-    for j in range(u-1,v):
-        k=k^l[j]
-    result.append(k)
-print(*result,sep="\n")
-        
+n,k = input().split()
+n,k = int(n), int(k)
+L = [ int(x) for x in input().split()]
+for i in range(0,k) :
+    a,b = input().split()
+    a,b = int(a), int(b)
+for i in range(0,k):
+    print(min(L[a-1:b]))
